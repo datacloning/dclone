e0b2fe61110eb356666c2641ff09e431d5c54e82
Peter Solymos <psolymos@gmail.com>
2017-02-17 23:28:34 -0700

    tests

---
+++ tests/examples.R
@@ -0,0 +1,45 @@
+#devtools::install_github("datacloning/dclone")
+library(dclone)
+
+## --- run examples with \dontrun sections ---
+
+help_pages <- c(
+    "clusterSize",
+    "clusterSplitSB",
+    "codaSamples",
+    "coef.mcmc.list",
+    "dc.fit",
+    "dc.parfit",
+    "dcFit",
+    "dclone",
+    "DcloneEnv",
+    "dcoptions",
+    "dctable",
+    "errlines",
+    "evalParallelArgument",
+    "jags.fit",
+    "jags.parfit",
+    "jagsModel",
+    "lambdamax.diag",
+    "make.symmetric",
+    "mclapplySB",
+    "mcmcapply",
+    "nclones",
+    "ovenbird",
+    "pairs.mcmc.list",
+    "parallel.inits",
+    "parCodaSamples",
+    "parDosa",
+    "parJagsModel",
+    "parLoadModule",
+    "parSetFactory",
+    "parUpdate",
+    "regmod",
+    "update.mcmc.list",
+    "write.jags.model")
+
+for (i in help_pages) {
+    cat("\n\n---------- dclone example:", i, "----------\n\n")
+    eval(parse(text=paste0("example(", i,
+        ", package = 'dclone', run.dontrun = TRUE)")))
+}
