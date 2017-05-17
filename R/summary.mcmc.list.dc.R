91f31943f87c055b0f4b067814e3b199553cee70
Peter Solymos <psolymos@gmail.com>
2017-05-17 13:05:30 -0600

    catch error in summary
    this fixes issue #8

--- R/summary.mcmc.list.dc.R
+++ R/summary.mcmc.list.dc.R
@@ -7,11 +7,18 @@ function(object, quantiles = c(0.025, 0.25, 0.5, 0.75, 0.975), ...)
     if (nchain(object) > 1) {
         abin <- getOption("dcoptions")$autoburnin
         rhat <- gelman.diag(object, autoburnin = abin)$psrf[,1]
+        if (inherits(rhat, "try-error"))
+            rhat <- NA
     } else rhat <- NA
     if (nvar(object) > 1) {
-        out$statistics <- cbind(out$statistics[,1:2], "DC SD"=dcsd(object), out$statistics[,3:4], "R hat" = rhat)
+        out$statistics <- cbind(out$statistics[,1:2],
+            "DC SD"=dcsd(object),
+            out$statistics[,3:4],
+            "R hat" = rhat)
     } else {
-        tmp <- c(out$statistics[1:2], "DC SD"=dcsd(object), out$statistics[3:4], "R hat" = rhat)
+        tmp <- c(out$statistics[1:2], "DC SD"=dcsd(object),
+            out$statistics[3:4],
+            "R hat" = rhat)
         out$statistics <- matrix(tmp, nrow=1)
         dimnames(out$statistics) <- list(varnames(object), names(tmp))
     }
