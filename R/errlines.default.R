9c512e16d9473c4047f285d877a5f7f66da43da4
Peter Solymos <psolymos@gmail.com>
2019-03-21 21:28:54 -0600

    fixing >1 logical issue

--- R/errlines.default.R
+++ R/errlines.default.R
@@ -1,16 +1,16 @@
 errlines.default <-
 function (x, y, type = "l", code = 0, width = 0, vertical = TRUE, col=1, bg = NA, ...)
 {
-    if (NCOL(y) == 1 && length(y == 2) && length(x) == 1) 
+    if (NCOL(y) == 1 && length(y) == 2 && length(x) == 1)
         y <- matrix(y, 1, 2)
-    if (NCOL(y) != 2) 
+    if (NCOL(y) != 2)
         stop("'y' must have 2 columns")
     type <- match.arg(substr(type, 1, 1), c("l", "b"))
     n <- length(x)
     y <- cbind(pmin(y[, 1], y[, 2]), pmax(y[, 1], y[, 2]))
-    if (type == "b" && width == 0) 
+    if (type == "b" && width == 0)
         type <- "l"
-    if (!(code %in% 0:3)) 
+    if (!(code %in% 0:3))
         stop("'code' value is inappropriate")
     x1 <- x - width/2
     x2 <- x + width/2
@@ -30,14 +30,14 @@ function (x, y, type = "l", code = 0, width = 0, vertical = TRUE, col=1, bg = NA
     }
     else {
         for (i in 1:n) {
-            if (code %in% c(1, 3)) 
+            if (code %in% c(1, 3))
                 if (vertical) {
                   lines(c(x1[i], x2[i]), rep(y[i, 1], 2), col=col[i], ...)
                 }
                 else {
                   lines(rep(y[i, 1], 2), c(x1[i], x2[i]), col=col[i], ...)
                 }
-            if (code %in% c(2, 3)) 
+            if (code %in% c(2, 3))
                 if (vertical) {
                   lines(c(x1[i], x2[i]), rep(y[i, 2], 2), col=col[i], ...)
                 }
