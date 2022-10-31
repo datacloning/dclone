7c309fadc3263e35bb784bc5c9507e6cdeb54859
Peter Solymos <psolymos@gmail.com>
2022-10-30 20:13:18 -0600

    Fix dctr issue with k=1 case
    Signed-off-by: Peter Solymos <psolymos@gmail.com>

--- R/dclone.dctr.R
+++ R/dclone.dctr.R
@@ -1,6 +1,8 @@
 dclone.dctr <-
 function(x, n.clones = 1, attrib = TRUE, ...)
 {
+    if (n.clones == 1)
+        return(x)
     rval <- t(dclone::dclone.default(t(x), n.clones, attrib, ...))
     attr(attr(rval, "n.clones"), "method") <- "tr"
     rval
